e120f61dae349f1f32b2868b33a409ac2d6ac776
Silvia Ulenberg <kontakt@silviaulenberg.de>
2017-10-06 10:23:43 +0200

    Fixes for newer ACF and wordpress versions

--- acf-relationship-multisite.php
+++ acf-relationship-multisite.php
@@ -4,29 +4,76 @@ Plugin Name: Advanced Custom Fields: Relationship Multisite
 Plugin URI: https://github.com/tmconnect/acf-relationship-multisite
 Description: Get post, pages and custom post types from another site of your WordPress Multisite installation. This plugin needs the installation/activation of Advanced Custom Fields V5.
 Version: 1.1.02 (>= ACF 5.2.7)
-Author: Thomas Meyer
-Author URI: www.dreihochzwo.de
+Author: Thomas Meyer, Silvia Ulenberg
+Author URI: www.dreihochzwo.de, https://www.silviaulenberg.de
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-
-
-
-// 1. set text domain
-// Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-load_plugin_textdomain( 'acf-relationship-multisite', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
-
-
-// 2. Include field type for ACF5
-// $version = 5 and can be ignored until ACF6 exists
-function include_field_types_relationship_multisite( $version ) {
-	
-	include_once('acf-relationship-multisite-v5.php');
-	
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-add_action('acf/include_field_types', 'include_field_types_relationship_multisite');	
+if ( ! class_exists( 'acf_plugin_FIELD_NAME' ) ) :
+	class acf_relationship_multisite {
+		/*
+		*  __construct
+		*
+		*  This function will setup the class functionality
+		*
+		*  @type	function
+		*  @date	17/02/2016
+		*  @since	1.0.0
+		*
+		*  @param	n/a
+		*  @return	n/a
+		*/
 
-	
-?>
+		function __construct() {
+
+			// vars
+			$this->settings = array(
+				'version' => '1.0.0',
+				'url'     => plugin_dir_url( __FILE__ ),
+				'path'    => plugin_dir_path( __FILE__ )
+			);
+
+			// set text domain
+			// https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
+			load_plugin_textdomain( 'acf-relationship-multisite', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+
+			// include field
+			add_action( 'acf/include_field_types', array( $this, 'include_field_types' ) ); // v5
+			add_action( 'acf/register_fields', array( $this, 'include_field_types' ) ); // v4
+
+		}
+
+		/*
+		*  include_field_types
+		*
+		*  This function will include the field type class
+		*
+		*  @type	function
+		*  @date	17/02/2016
+		*  @since	1.0.0
+		*
+		*  @param	$version (int) major ACF version. Defaults to false
+		*  @return	n/a
+		*/
+
+		function include_field_types( $version = false ) {
+
+			// support empty $version
+			if ( ! $version ) {
+				$version = 5;
+			}
+
+			// include
+			include_once( 'acf-relationship-multisite-v' . $version . '.php' );
+		}
+	}
+
+// initialize
+	new acf_relationship_multisite();
+// class_exists check
+endif;
